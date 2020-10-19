46ef9eef54c1def59dc75cf22360a55d59594db4
Nicholas-Schaub <nick.schaub@axleinfo.com>
2020-10-19 11:14:36 -0400

    Changed wheel directory in build workflow

--- .github/workflows/actions/build.sh
+++ .github/workflows/actions/build.sh
@@ -8,7 +8,7 @@ for PYTHON_VERSION in ${PYTHON_VERSIONS[@]}; do
     /opt/python/${PYTHON_VERSION}/bin/pip install --upgrade pip
     /opt/python/${PYTHON_VERSION}/bin/pip install -U wheel auditwheel
     /opt/python/${PYTHON_VERSION}/bin/pip install numpy==1.19.2 cython==3.0a1
-    /opt/python/${PYTHON_VERSION}/bin/python setup.py bdist_wheel -d wheelhouse
+    /opt/python/${PYTHON_VERSION}/bin/python setup.py bdist_wheel -d dist
 done
 
 for whl in ./dist/*.whl; do
