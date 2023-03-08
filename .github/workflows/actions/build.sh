c44b5fcf31331e06fdaad553ed7d747d3f0385aa
Nicholas-Schaub <nicholas.j.schaub@gmail.com>
2023-03-08 10:16:40 -0500

    Reconfigured git actions

--- .github/workflows/actions/build.sh
+++ .github/workflows/actions/build.sh
@@ -6,9 +6,8 @@ PYTHON_VERSIONS=("cp38-cp38" "cp39-cp39" "cp310-cp310")
 
 for PYTHON_VERSION in ${PYTHON_VERSIONS[@]}; do
     /opt/python/${PYTHON_VERSION}/bin/pip install --upgrade pip
-    /opt/python/${PYTHON_VERSION}/bin/pip install -U wheel auditwheel
-    /opt/python/${PYTHON_VERSION}/bin/pip install numpy==1.19.2 cython==3.0a1
-    /opt/python/${PYTHON_VERSION}/bin/python setup.py bdist_wheel -d dist
+    /opt/python/${PYTHON_VERSION}/bin/pip install -U wheel auditwheel poetry
+    /opt/python/${PYTHON_VERSION}/bin/python -m poetry build
 done
 
 for whl in ./dist/*.whl; do
